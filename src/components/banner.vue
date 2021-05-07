85f79b6c41a66081f48776141e2f97d4fda64678
DESKTOP-KL0E6A7\hjt-zy <315862477@qq.com>
2021-05-07 09:48:27 +0800

    feat: project init

---
+++ src/components/banner.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <a :href="obj.link">
+            <img
+                :src="obj.src"
+                alt="图片"
+                width="100%"
+            >
+        </a>
+    </div>
+</template>
+
+<script>
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'Banner',
+        props: {
+            obj: {
+                type: Object,
+                default: () => ({
+                    link: '',
+                    src: '',
+                }),
+            },
+        },
+        setup() {},
+    });
+</script>
+
+<style></style>
